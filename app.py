d4a5ce9fa21152469b5997f97157055186c4a404
Pucela83 <vane_euro@hotmail.com>
2023-06-08 20:40:42 +0200

    quitar bd

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
 from flask import *
 from flask_bootstrap import Bootstrap
-from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-mysql = MySQL()
 
-# Configuración de la conexión a la base de datos
-app.config['MYSQL_DATABASE_HOST'] = 'Pucelana83.mysql.pythonanywhere-services.com'
-app.config['MYSQL_DATABASE_USER'] = 'Pucelana83'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Z@p@titos83'
-app.config['MYSQL_DATABASE_DB'] = 'Pucelana83$default'
-
-mysql.init_app(app)
 
 @app.route('/')
 def index():
